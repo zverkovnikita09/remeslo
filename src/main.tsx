391e8219cb70e8f44ccc7561067f052c42e9fbcc
Mikhail Lisovskii <f1ste@yandex.ru>
2023-10-28 23:07:28 +0300

    add error boundary, fetch categories, fix return type in api

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import ErrorBoundary from './app/providers/errorBoundary.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
+  <ErrorBoundary
+    fallback={<p>Произошла ошибка</p>}
+  >
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </ErrorBoundary>,
 )
