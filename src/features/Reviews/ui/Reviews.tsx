152032efa85cad242feeb7b8cd954c5ffd057b4f
Mikhail Lisovskii <f1ste@yandex.ru>
2023-12-01 02:09:41 +0300

    creating reviews popup component

---
+++ src/features/Reviews/ui/Reviews.tsx
@@ -0,0 +1,58 @@
+import { Button } from 'src/shared/ui/Button/Button'
+import style from './Reviews.module.scss'
+import { useState } from 'react'
+import { ReviewsPopup } from './ReviewsPopup/ReviewsPopup'
+import { labelsCounterFormatter } from 'src/shared/lib/labelsCounterFormatter/labelsCounterFormatter'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import { IUser, ProfileInfo } from 'src/app/providers/AuthProvider'
+import { getData } from 'src/shared/lib/api/api'
+
+interface ReviewsProps {
+  marks?: number
+  overall_rating?: number
+}
+
+export interface GoodEstimaions {
+  estimation: number
+  id: string
+  user: IUser
+  profile: ProfileInfo
+}
+
+export const Reviews = ({marks, overall_rating}: ReviewsProps) => {
+  const [isPopupActive, setIsPopupActive] = useState(false)
+
+  const { slug } = useParams();
+  const { data: dataEstimations } = useQuery({
+    queryKey: 'goodEstimations',
+    queryFn: () => getData<GoodEstimaions[]>({
+      url: `/api/v1/mark?markable_id=${slug}`,
+      dataFlag: true
+    }),
+  })
+
+  const openPopup = () => {
+    setIsPopupActive(true)
+  }
+
+  const closePopup = () => {
+    setIsPopupActive(false)
+  }
+
+  return (
+    <>
+      <ReviewsPopup
+      isActive={isPopupActive} 
+      closePopup={closePopup}
+      overall_rating={overall_rating}
+      marks={marks}
+      dataEstimations={dataEstimations}
+      />
+      <Button className={style.reviews} onClick={openPopup}>
+            {marks ? labelsCounterFormatter(marks, ['Отзыв', 'Отзыва', 'Отзывов']) : 'нет отзывов'}
+      </Button>
+
+    </>
+  )
+}
---
+++ src/features/Reviews/ui/ReviewsPopup/ReviewsPopup.tsx
@@ -0,0 +1,40 @@
+import { Popup } from 'src/shared/ui/Popup/Popup'
+import style from './ReviewsPopup.module.scss'
+import { Title, TitleSize } from 'src/shared/ui/Title/TItle'
+import { CloseButton } from 'src/shared/ui/CloseButton/CloseButton'
+import { Button } from 'src/shared/ui/Button/Button'
+import { OverallRating } from 'src/shared/ui/OverallRating/OverallRating'
+import { ReviewsCount } from 'src/shared/ui/ReviewsStats/ReviewsCount'
+import { GoodEstimaions } from '../Reviews'
+
+interface ReviewsPopupProps {
+  isActive: boolean
+  closePopup: () => void
+  overall_rating?: number
+  marks?: number
+  dataEstimations?: GoodEstimaions[]
+}
+
+export const ReviewsPopup = ({ marks, overall_rating, closePopup, isActive, dataEstimations }: ReviewsPopupProps) => {
+
+
+  return (
+    <Popup isActive={isActive} closePopup={closePopup}>
+      <div className={style.reviewsPopup}>
+        <CloseButton className={style.reviewsPopup__close} />
+        <Title size={TitleSize.S}>Отзывы о товаре</Title>
+        <div className={style.reviewsPopup__ratingInfo}>
+          <OverallRating
+            overall_rating={overall_rating}
+            marks={marks}
+          />
+          <Button className={style.reviewsPopup__newOpinion}>Оставить отзыв</Button>
+        </div>
+        <div className={style.reviewsPopup__reviewStats}>
+          <ReviewsCount stats={dataEstimations} />
+        </div>
+
+      </div>
+    </Popup>
+  )
+}
