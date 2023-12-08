010679666da618a4530a4742f55bf59f8d17110a
Mikhail Lisovskii <f1ste@yandex.ru>
2023-12-08 11:59:42 +0300

    add grey style to button

--- src/features/ShareButton/ui/ShareButton.tsx
+++ src/features/ShareButton/ui/ShareButton.tsx
@@ -1,10 +1,13 @@
-import { Button } from "src/shared/ui/Button/Button"
+import { Button, ButtonTheme } from "src/shared/ui/Button/Button"
 import { ShareIcon } from "src/shared/ui/ShareIcon/ShareIcon"
 import style from './ShareButton.module.scss'
 
 export const ShareButton = () => {
     return (
-        <Button className={style.shareButton}>
+        <Button
+            className={style.shareButton}
+            theme={ButtonTheme.GREY}
+        >
             <ShareIcon />
         </Button>
     )
