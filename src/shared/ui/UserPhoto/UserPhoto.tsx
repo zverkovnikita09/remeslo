921cd55a520341bd7a8040f7c27c69c9bb8c4b4d
Mikhail Lisovskii <f1ste@yandex.ru>
2023-12-09 14:40:58 +0300

    add template user image

--- src/shared/ui/UserPhoto/UserPhoto.tsx
+++ src/shared/ui/UserPhoto/UserPhoto.tsx
@@ -1,6 +1,7 @@
 import { ProfileInfo } from 'src/app/providers/AuthProvider'
 import style from './UserPhoto.module.scss'
 import { classNames } from 'src/shared/lib/classNames/classNames'
+import userImg from './assets/user.svg'
 
 export enum ImageSize {
     L = 'size_l',
@@ -16,11 +17,7 @@ interface UserPhotoProps {
 export const UserPhoto = ({ profileInfo, imageSize = ImageSize.M }: UserPhotoProps) => {
     return (
         <div className={classNames(style.imageContainer, {}, [style[imageSize]])}>
-            {
-                profileInfo?.avatar ?
-                    <img src={profileInfo.avatar} alt="Фото профиля" className={style.imageContainer__image} />
-                    : `${profileInfo?.firstname[0] ?? ''}${profileInfo?.lastname[0] ?? ''}`
-            }
+            <img src={profileInfo?.avatar ? profileInfo.avatar : userImg} alt="Фото профиля" className={style.imageContainer__image} />
         </div>
     )
 }
