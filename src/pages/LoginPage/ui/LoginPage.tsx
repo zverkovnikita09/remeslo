d5d62b217e14a6fc92abaec3f4cd62b8628341e0
Mikhail Lisovskii <f1ste@yandex.ru>
2023-12-23 23:59:20 +0300

    popup bug fix

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -4,7 +4,6 @@ import style from './LoginPage.module.scss'
 import background from '../assets/login_background.png'
 import { GreyText } from 'src/shared/ui/GreyText/GreyText'
 import { Outlet } from 'react-router-dom'
-import { Select } from 'src/shared/ui/Select/Select'
 
 export const LoginPage = () => {
   return (
@@ -12,7 +11,6 @@ export const LoginPage = () => {
       <div className={style.loginPage__content}>
         <MainLogo />
         <Outlet />
-{/*         <Select /> */}
         <GreyText>© 2023 Remeslo</GreyText>
       </div>
       <div className={style.loginPage__image}>
