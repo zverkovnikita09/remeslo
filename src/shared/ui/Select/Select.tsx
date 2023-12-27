7c4368923fb3afa50c310a411bb82ce2e7ef173d
Mikhail Lisovskii <f1ste@yandex.ru>
2023-12-27 22:46:05 +0300

    refactor select

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -6,22 +6,27 @@ import { SlArrowDown } from "react-icons/sl";
 import { classNames } from "src/shared/lib/classNames/classNames";
 import { IoClose } from "react-icons/io5";
 import { Button } from "../Button/Button";
+import { useToggleDropdown } from "src/shared/hooks/useToggleDropdown";
 
+interface OptionType {
+    name: string
+    value: string
+}
 
 interface CommonSelectProps {
-    options: (string | { name: string; value: string; })[]
+    options: (string | OptionType)[]
     placeholder?: string
 }
 
 interface MultipleSelectProps {
     multiple: true
-    value: string[]
+    value?: string[]
     setValue: (value: string[]) => void
 }
 
 interface SingleSelectProps {
     multiple?: false
-    value: string
+    value?: string
     setValue: (value: string) => void
 }
 
@@ -31,17 +36,17 @@ export const Select = (props: SelectProps) => {
     const { multiple, placeholder, value, setValue, options } = props
 
     const [, setIsFocused] = useState(false);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isDropdownOpen, toggleDropdown] = useToggleDropdown();
     const [availableOptions, setAvailableOptions] = useState(options);
     const elementRef = useRef<HTMLDivElement>(null)
 
-    const isOptionStringArray = (options: (string | { name: string; value: string; })[]): options is string[] => {
+    const isOptionStringArray = (options: (string | OptionType)[]): options is string[] => {
         return !!(options?.length && typeof options[0] === 'string')
     }
 
     const addValue = useCallback((selectValue: string) => () => {
         if (multiple) {
-            setValue([...value, selectValue]);
+            setValue([...(value ?? []), selectValue]);
             setAvailableOptions(prev => prev.filter(option => {
                 if (typeof option === 'string') {
                     return selectValue !== option;
@@ -49,80 +54,91 @@ export const Select = (props: SelectProps) => {
 
                 return selectValue !== option.value;
             }))
+            toggleDropdown();
             return;
         }
 
-        setIsDropdownOpen(false)
         setValue(selectValue);
     }, [value])
 
     const deleteValue = useCallback((selectValue: string) => () => {
         if (multiple) {
-            setValue(value.filter(item => item !== selectValue))
+            setValue(value?.filter(item => item !== selectValue) ?? [])
 
             const optionValue = isOptionStringArray(options)
                 ? selectValue
-                : options.find(option => selectValue === (option as { name: string; value: string; }).value);
+                : options.find(option => selectValue === (option as OptionType).value);
             setAvailableOptions(prev => [...prev, optionValue!]);
         }
     }, [value])
 
+    const valueToShow = (value: string) => {
+        if (isOptionStringArray(options)) {
+            return value
+        }
+
+        return (options.find(item => (item as OptionType).value === value) as OptionType)?.name || ''
+    }
+
     return (
-        <div
-            className={style.select}
-            tabIndex={0}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-            ref={elementRef}
-            onClick={() => setIsDropdownOpen(pr => !pr)}
-        >
-            <SlArrowDown
-                className={classNames(style.select__arrowDown, { [style.rotated]: isDropdownOpen })}
-                color="#9B9B9B"
-            />
-            {!multiple && (value || placeholder)}
-            {multiple && (value.length ?
-                (value.map((item) => (
-                    <div key={item} className={style.select__valueItem} onClick={e => e.stopPropagation()}>
-                        {item}
-                        <Button onClick={deleteValue(item)}><IoClose /></Button>
-                    </div>
-                )))
-                : placeholder)}
-            <Dropdown
-                isOpen={isDropdownOpen}
-                onClose={() => setIsDropdownOpen(false)}
-                targetRef={elementRef}
-                horizontalPosition="left"
-                width="100%"
-                className={style.select__dropdown}
+        <div className={style.select}>
+            <div
+                className={style.select__toggler}
+                tabIndex={0}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                ref={elementRef}
+                onClick={toggleDropdown}
             >
-                {availableOptions?.length
-                    ? availableOptions.map((option) => {
-                        if (typeof option === "string") {
+                <SlArrowDown
+                    className={classNames(style.select__arrowDown, { [style.rotated]: isDropdownOpen })}
+                    color="#9B9B9B"
+                />
+                {!multiple && (valueToShow(value ?? '') || placeholder)}
+                {multiple && (value?.length ?
+                    (value.map((item) => (
+                        <div key={item} className={style.select__valueItem} onClick={e => e.stopPropagation()}>
+                            {valueToShow(item)}
+                            <Button onClick={deleteValue(item)}><IoClose /></Button>
+                        </div>
+                    )))
+                    : placeholder)}
+                <Dropdown
+                    isOpen={isDropdownOpen}
+                    onClose={toggleDropdown}
+                    targetRef={elementRef}
+                    horizontalPosition="left"
+                    width="100%"
+                    className={style.select__dropdown}
+                >
+                    {availableOptions?.length
+                        ? availableOptions.map((option) => {
+                            if (typeof option === "string") {
+                                return (
+                                    <div
+                                        key={option}
+                                        className={classNames(style.select__item, { [style.selected]: option === value })}
+                                        onClick={addValue(option)}
+                                    >
+                                        {option}
+                                    </div>
+                                )
+                            }
                             return (
                                 <div
-                                    key={option}
-                                    className={classNames(style.select__item, { [style.selected]: option === value })}
-                                    onClick={addValue(option)}
+                                    key={option.name}
+                                    className={classNames(style.select__item, { [style.selected]: option.value === value })}
+                                    onClick={addValue(option.value)}
                                 >
-                                    {option}
+                                    {option.name}
                                 </div>
                             )
-                        }
-                        return (
-                            <div
-                                key={option.name}
-                                className={classNames(style.select__item, { [style.selected]: option.value === value })}
-                                onClick={addValue(option.value)}
-                            >
-                                {option.name}
-                            </div>
-                        )
-                    })
-                    : <GreyText className={style.select__text}>Нет доступных элементов</GreyText>
-                }
-            </Dropdown>
+                        })
+                        : <GreyText className={style.select__text}>Нет доступных элементов</GreyText>
+                    }
+                </Dropdown>
+            </div>
+
         </div>
     )
 }
